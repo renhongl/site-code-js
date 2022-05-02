dace17d625ff4d2cd5d5abe426d2e7bfb62e64d5
renhongl <1075220132@qq.com>
2022-05-02 22:34:17 +0800

    1 file modified, 6 added

--- utils/call.js
+++ utils/call.js
@@ -1,1 +1,12 @@
-export function myCall() {}
+/**
+ *
+ */
+export function myCall(...args) {
+  const [context, params] = args;
+  const Fn = () => {};
+  Fn.prototype = Object.create(context);
+}
+
+function sum() {}
+
+sum.call({ x: 1, y: 2 }, 3, 4);
